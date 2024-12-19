8f55d83cbe40d9549fff3b16a09cab52628f8062
superznake <superznake@yandex.ru>
2024-12-19 19:44:22 +0300

    .

--- example_algorithm.py
+++ example_algorithm.py
@@ -24,29 +24,38 @@ def calc_curr_bal(used: List[Event]):
 def analyzer(curr_bal: List[int]):
     result = False
     sum = 0
-    for i in curr_bal:
-        if i:
-            ...
+    for i in range(4):
+        sum += curr_bal[i]
+        if result:
+            if curr_bal[i] > result[1]:
+                result = [i, curr_bal[i]]
+        elif curr_bal[i] > 2:
+            result = [i, curr_bal[i]]
     return result
 
 
 def finder(param: int, value: int, possibles: List[Event]):
-    variants: Dict = {"Value": 0, "IDs": [0]}
+    variants: Dict = {"Value": possibles[0].bal[param], "IDs": [possibles[0].id]}
+    delta = variants["Value"] + value
     for possible in possibles:
-        if abs(variants.get("Value")-value) > 0:
-            if possible.bal[param] :
-                ...
-    result = variants
+        if abs(possible.bal[param]+value) == abs(delta):
+            variants["IDs"].append(possible.id)
+        elif abs(possible.bal[param]+value) < abs(delta):
+            variants["Value"] = possible.bal[param]
+            variants["IDs"].clear()
+            variants["IDs"].append(possible.id)
+            delta = variants["Value"] + value
+    result = variants["IDs"]
     return result
 
 
 def balancer(current: int, max: int, possible: List[Event], used: List[Event]):
-
     result = "error or smth"
     if current >= math.ceil(max/2):
-        if analyzer(calc_curr_bal(used)):
+        analyzed = analyzer(calc_curr_bal(used))
+        if analyzed:
             variants: List[int] = []
-            result = finder()
+            result = finder(param=analyzed[0], value=analyzed[1], possibles=possible)
         else:
             result = "it's ok already"
     else:
@@ -54,4 +63,4 @@ def balancer(current: int, max: int, possible: List[Event], used: List[Event]):
     return result
 
 
-print(balancer(current_round,max_round,events[0],events[1]))
+print(balancer(current_round, max_round, events[0], events[1]))
